170489b2915a20a2c007f1c52abc9b6de8e97f8e
3-shravan <shantanuyadav379@gmail.com>
2024-12-28 02:56:02 +0530

    user registration at server

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
-const cors = require('cors')
 const app = express();
+
+const cors = require('cors')
+const connectToDatabse = require('./db/db')
+const userRoutes = require('./routes/user.routes')
+
+connectToDatabse();
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 
 
 app.get('/', (req, res) => {
    res.send('helo World')
 })
 
-
-
-
+app.use('/', userRoutes)
 
 module.exports = app
