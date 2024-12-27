4e84860835dbe7eab5e43eab58cd10825c5cdc6e
3-shravan <shantanuyadav379@gmail.com>
2024-12-28 00:24:28 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const cors = require('cors')
+const app = express();
+
+
+app.get('/', (req, res) => {
+   res.send('helo World')
+})
+
+
+
+
+
+module.exports = app
