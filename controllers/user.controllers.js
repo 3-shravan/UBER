dda798ac5c00c294943cad5f544ea0f539343554
3-shravan <shantanuyadav379@gmail.com>
2024-12-28 18:14:30 +0530

    user login at server

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -10,7 +10,7 @@ module.exports.registerUser = async (req, res, next) => {
    if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() })
    }
-  
+
 
    const { fullname, email, password } = req.body
 
@@ -24,5 +24,31 @@ module.exports.registerUser = async (req, res, next) => {
    })
    const token = await user.generateAuthToken();
 
-   res.status(201).json({ token, user })
+   res.status(201).json({ token, user, message: 'Registered Successfully' })
+}
+
+module.exports.loginUser = async (req, res, next) => {
+
+   const errors = validationResult(req)
+
+   if(!errors.isEmpty()) {
+      return res.status(401).json({ errors: erros.array() })
+   }
+
+   const {email , password}=req.body;
+
+   const user = await userModel.findOne({email}).select('+password');
+
+   if(!user){
+      return res.status(401).json({message:'invalid email or password'})
+   }
+
+   const isMatch=await user.comparePassword(password);
+   console.log(isMatch);
+
+   if(!isMatch){
+      return res.status(401).json({message:'invalid email or password'})
+   }
+   const token =await user.generateAuthToken();
+   res.status(200).json({token,user ,message:'Login Successfull'})
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,11 +7,17 @@ const userController = require('../controllers/user.controllers');
 
 router.post('/register',
    [
-      body('fullname.firstname').isLength({ min: 3 }).withMessage('First name must be at least 3 characters'),
-      body('email').isEmail().withMessage('Please enter a valid email address'),
-      body('password').isLength({ min: 2 }).withMessage('Password must be at least 2 characters')
+      body('fullname.firstname').trim().isLength({ min: 3 }).withMessage('First name must be at least 3 characters'),
+      body('email').trim().isEmail().withMessage('Please enter a valid email address'),
+      body('password').trim().isLength({ min: 2 }).withMessage('Password must be at least 2 characters')
    ],
    userController.registerUser
 )
+
+router.post('/login', [
+   body('email').trim().isEmail().withMessage('Please enter a valid email address'),
+   body('password').trim().isLength({ min: 2 }).withMessage('Password must be at least 2 characters')
+
+], userController.loginUser)
 
 module.exports = router;
